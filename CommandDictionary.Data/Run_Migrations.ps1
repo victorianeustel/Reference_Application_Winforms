a04500693328da6bb83545099b5c3b0748c6e0c5
vneustel <vneustel@bell.bank>
2024-12-02 14:24:15 -0600

    Saving run migration script

--- CommandDictionary.Data/Run_Migrations.ps1
+++ CommandDictionary.Data/Run_Migrations.ps1
@@ -1,9 +1,13 @@
 ﻿(Get-Location).path
+Write-Host "`n"
+
 try {
     dotnet ef database update
 }
 catch{
-    "An error occurred while attempting to run migrations"
+    "An error occurred while attempting to run migrations"  
+    Write-Host $_
+    Write-Host "`n"
+    Read-Host "Press Enter to exit..."
 }
 
-Read-Host "Press Enter to exit..."
